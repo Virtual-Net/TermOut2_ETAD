5b86ca8be58202ff89fe0299292a7376e09f7c44
Petran <p.p.tsironis@gmail.com>
2021-05-28 13:51:53 +0300

    path in launcher

--- launcher.sh
+++ launcher.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 #launcher.sh
 #navigate to home directory, then to this directory, then execute python script, then back home
-log="/home/pi/AutoPark2020_optimization/logs/$(date +%Y-%m-%d)_launch.log"
+log="/home/pi/Autopark2020_Exit/logs/$(date +%Y-%m-%d)_launch.log"
 echo $(date): Starting launcher.sh | tee -a "$log"
 
 cd /
-cd home/pi/AutoPark2020_optimization
+cd home/pi/Autopark2020_Exit
 sudo python3 ExitTerminal.py
 cd /
 
